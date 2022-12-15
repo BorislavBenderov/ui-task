c4a47d2d7ad5c6eaf80377166d37bfa210413a4b
Borislav Benderov <108291221+BorislavBenderov@users.noreply.github.com>
2022-12-15 19:37:37 +0200

    Fetch data from server

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from "react";
 import { Content } from "./components/Content";
+import { onSnapshot, collection } from 'firebase/firestore';
+import { database } from './firebaseConfing';
 
 function App() {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    onSnapshot(collection(database, 'articles'), (snapshot) => {
+      setArticles(snapshot.docs.map((item) => {
+        return { ...item.data(), id: item.id }
+      }))
+    })
+  }, []);
+
+
   return (
     <div className="App">
-      <Content />
+      <Content articles={articles}/>
     </div>
   );
 }
